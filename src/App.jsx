2766901954d9cb66efd4469b47adfbb7273c1f58
Segara Bhagas Dagsapurwa <segara2410@gmail.com>
2021-04-08 15:41:30 +0700

    add action and pose sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
+import Input from './components/Input';
+
+import Button from '@material-ui/core/Button';
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import MuiAppBar from "@material-ui/core/AppBar";
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import MuiToolbar from "@material-ui/core/Toolbar";
 import MuiTypography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import MUIDataTable from "mui-datatables";
 
-const useStyles = makeStyles(() => ({
+import AddIcon from '@material-ui/icons/Add';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+  button: {
+    margin: theme.spacing(1),
+  },
 }));
 
-const columns = [
-  { 
-    field: 'id', 
-    label: 'ID', 
+const actionColumns = [
+  {
+    name: "id",
+    label: "#",
     options: {
       filter: true,
       sort: true,
@@ -30,98 +38,105 @@ const columns = [
     options: {
       filter: true,
       sort: true,
-      customBodyRender: (value, tableMeta, updateValue) => (
-        <FormControlLabel
-          value={value}
-          control={<TextField value={value} />}
-          onChange={event => updateValue(event.target.value)}
-        />
-      )
     }
   },
   {
-    field: 'speed',
-    label: 'Speed',
+    name: "next",
+    label: "Next",
     options: {
       filter: false,
       sort: false,
-      customBodyRender: (value, tableMeta, updateValue) => {
-        const inputProps = {
-          min: 0,
-          max: 1,
-          step: 0.01,
-        };
-
-        return (
-          <FormControlLabel
-            control={
-              <TextField 
-                value={value || ''} 
-                type='number' 
-                inputProps={inputProps}
-              />}
-            onChange={event => updateValue(event.target.value)}
-          />
-        )
-      }
-    }
-  },
-  {
-    field: 'pause',
-    label: 'Pause',
-    options: {
-      filter: false,
-      sort: false,
-      
-      customBodyRender: (value, tableMeta, updateValue) => {
-        const inputProps = {
-          min: 0,
-          max: 100,
-          step: 0.1,
-        };
-
-        return (
-          <FormControlLabel
-            control={
-              <TextField 
-                value={value || ''} 
-                type='number' 
-                inputProps={inputProps}
-              />}
-            onChange={event => updateValue(event.target.value)}
-          />
-        )
-      }
     }
   },
 ];
+ 
+const actionData = [
+  { id: 1, name: 'action_1', next: 0 },
+  { id: 2, name: 'action_2', next: 0 },
+  { id: 3, name: 'action_3', next: 0 },
+  { id: 4, name: 'action_4', next: 0 },
+  { id: 5, name: 'action_5', next: 0 },
+  { id: 6, name: 'action_6', next: 0 },
+  { id: 7, name: 'action_7', next: 0 },
+  { id: 8, name: 'action_8', next: 0 },
+  { id: 9, name: 'action_9', next: 0 },
+  { id: 10, name: 'action_10', next: 0 },
+]; 
 
-const rows = [
-  [1, 'motion_1', 0.35, 0.3 ],
-  [2, 'motion_2', 0.42, 0.4 ],
-  [3, 'motion_3', 0.45, 0.4 ],
-  [4, 'motion_4', 0.16, 0.1 ],
-  [5, 'motion_5', 0.22, 0.2 ],
-  [6, 'motion_6', 0.15, 0.1 ],
-  [7, 'motion_7', 0.44, 0.4 ],
-  [8, 'motion_8', 0.36, 0.3 ],
-  [9, 'motion_9', 0.65, 0.6 ],
-];
-
-const options = {
-  tableBodyMaxHeight: '400px',
+const actionOptions = {
+  tableBodyMaxHeight: '558px',
   pagination: false,
   filter: false,
   download: false,
   print: false,
-  search: false,
-  searchOpen: true,
+  search: true,
   selectableRows: 'single',
   sortOrder: {
     name: 'ID',
     direction: 'asc'
   },
-  selectableRowsOnClick: true
+  selectableRowsOnClick: true,
+  selectableRowsHideCheckboxes: true
+};
+
+const poseColumns = [
+  {
+    name: "id",
+    label: "#",
+    options: {
+      filter: true,
+      sort: false,
+      customBodyRender: (rowIndex, dataIndex) => dataIndex.rowIndex + 1 
+    }
+  },
+  {
+    name: "name",
+    label: "Name",
+    options: {
+      filter: true,
+      sort: true,
+    }
+  },
+  {
+    name: "speed",
+    label: "Speed",
+    options: {
+      filter: false,
+      sort: false,
+    }
+  },
+  {
+    name: "pause",
+    label: "Pause",
+    options: {
+      filter: false,
+      sort: false,
+    }
+  },
+];
+ 
+const poseData = [
+  { name: 'pose_1', speed: 0.5, pause: 1.5 },
+  { name: 'pose_2', speed: 0.5, pause: 1.5 },
+  { name: 'pose_3', speed: 0.5, pause: 1.5 },
+  { name: 'pose_4', speed: 0.5, pause: 1.5 },
+  { name: 'pose_4', speed: 0.5, pause: 1.5 },
+]; 
+
+const poseOptions = {
+  tableBodyMaxHeight: '269px',
+  pagination: false,
+  filter: false,
+  download: false,
+  print: false,
+  search: true,
+  selectableRows: 'single',
+  sortOrder: {
+    name: 'ID',
+    direction: 'asc'
+  },
+  selectableRowsOnClick: true,
+  selectableRowsHideCheckboxes: true
 };
 
 function App() {
@@ -135,10 +150,83 @@ function App() {
         </MuiToolbar>
       </MuiAppBar>
       <Card>
-        <CardContent style={{ width: '100%' }}>
+        <CardContent>
           <Grid container spacing={3}>
-            <Grid item md={12} lg={4}>
-              <MUIDataTable data={rows} columns={columns} options={options} />
+            <Grid item xs={12} md={6} lg={4}>
+              <MUIDataTable title={"Action List"} data={actionData} columns={actionColumns} options={actionOptions}/>
+            </Grid>
+            <Grid item xs={12} md={6} lg={4}>
+              <Card>
+                <CardContent>
+                  <MuiTypography variant="h6">
+                    Action
+                  </MuiTypography>
+                  <div style={{marginBottom: 10}}>
+                    <Input 
+                      id="pose-name" 
+                      label="Name" 
+                      width="66%"
+                    />
+                    <Input 
+                      id="pose-speed" 
+                      label="Next" 
+                      width="31%"
+                    />
+                  </div>
+
+                  <MUIDataTable title={"Pose List"} data={poseData} columns={poseColumns} options={poseOptions}/>
+                  
+                  <div style={{marginTop: 10, marginBottom: -10}}>
+                    <Button
+                      variant="contained"
+                      style={{background: '#11cb5f'}}
+                      color="primary"
+                      className={classes.button}
+                      startIcon={<AddIcon />}
+                    >
+                      Add
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="default"
+                      className={classes.button}
+                      startIcon={<ArrowUpwardIcon />}
+                    >
+                      Move Up
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="default"
+                      className={classes.button}
+                      startIcon={<ArrowDownwardIcon />}
+                    >
+                      Move Down
+                    </Button>
+                  </div>
+                </CardContent>
+              </Card>
+              <Card style={{marginTop: 10}}>
+                <CardContent>
+                  <MuiTypography variant="h6">
+                    Pose
+                  </MuiTypography>
+                    <Input 
+                      id="pose-name" 
+                      label="Name" 
+                      width="31%"
+                    />
+                    <Input 
+                      id="pose-speed" 
+                      label="Speed" 
+                      width="31%"
+                    />
+                    <Input 
+                      id="pose-pause" 
+                      label="Pause" 
+                      width="31%"
+                    />
+                </CardContent>
+              </Card>
             </Grid>
           </Grid>
         </CardContent>
---
+++ src/components/Input.jsx
@@ -0,0 +1,17 @@
+import TextField from '@material-ui/core/TextField';
+
+export default function Input({id, label, width}) {
+  return (
+    <TextField 
+      id={id} 
+      label={label}
+      variant="outlined"
+      margin="dense"
+      style={{margin: 3, marginTop: 20, width: width}}
+      InputLabelProps={{
+        shrink: true,
+      }}
+    />
+  )
+
+}
